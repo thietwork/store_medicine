d96e4e0406ccce5fe64b4e52f9ee6e8f8c81a9a2
thaivo <thaivo.software@gmail.com>
2023-08-09 09:59:30 +0700

    carts models

---
+++ src/main/java/com/models/Carts.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.models;
+
+/**
+ *
+ * @author thaiq
+ */
+public class Carts {
+    private int id;
+    private double name;
+    private double price; 
+    private int quantity; 
+    private double total;
+    private int user_id;
+    private String product_id;
+    
+    
+}
